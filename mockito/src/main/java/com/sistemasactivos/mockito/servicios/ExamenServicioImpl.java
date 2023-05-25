ebc645e257c4df63a48472eeae1ff1c00686fd44
tgriffabenitez <tgriffabenitez@gmail.com>
2023-05-25 17:40:46 -0300

    Implemento el metodo que busca el examen por las preguntas

--- mockito/src/main/java/com/sistemasactivos/mockito/servicios/ExamenServicioImpl.java
+++ mockito/src/main/java/com/sistemasactivos/mockito/servicios/ExamenServicioImpl.java
@@ -2,21 +2,37 @@ package com.sistemasactivos.mockito.servicios;
 
 import com.sistemasactivos.mockito.modelos.Examen;
 import com.sistemasactivos.mockito.repositorios.ExamenRepositorio;
+import com.sistemasactivos.mockito.repositorios.PreguntaRepositorio;
 
+import java.util.List;
 import java.util.Optional;
 
 public class ExamenServicioImpl implements ExamenServicio {
-    private ExamenRepositorio repositorio;
+    private ExamenRepositorio examenRepositorio;
+    private PreguntaRepositorio preguntaRepositorio;
 
-    public ExamenServicioImpl(ExamenRepositorio repositorio) {
-        this.repositorio = repositorio;
+    public ExamenServicioImpl(ExamenRepositorio repositorio, PreguntaRepositorio preguntaRepositorio) {
+        this.examenRepositorio = repositorio;
+        this.preguntaRepositorio = preguntaRepositorio;
     }
 
     @Override
     public Optional<Examen> findExamenPorNombre(String nombre) {
-        return repositorio.findAll()
+        return examenRepositorio.findAll()
                 .stream()
                 .filter(examen -> examen.getNombre().equals(nombre))
                 .findFirst();
+    }
+
+    @Override
+    public Examen findExamenPorNombreConPreguntas(String nombre) {
+        Optional<Examen> examenOptional = findExamenPorNombre(nombre);
+        Examen examen = null;
+        if (examenOptional.isPresent()){
+            examen = examenOptional.orElseThrow();
+            List<String> preguntas = preguntaRepositorio.findPreguntaPorExamenId(examen.getId());
+            examen.setPreguntas(preguntas);
+        }
+        return examen;
     }
 }
