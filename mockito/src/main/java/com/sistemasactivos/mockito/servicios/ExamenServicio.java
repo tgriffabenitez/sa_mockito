2ca3a8064b4a2cdda63627317399bd484679e86f
tgriffabenitez <tgriffabenitez@gmail.com>
2023-05-25 17:39:49 -0300

    Agrego el metodo para buscar las preguntas por examen

--- mockito/src/main/java/com/sistemasactivos/mockito/servicios/ExamenServicio.java
+++ mockito/src/main/java/com/sistemasactivos/mockito/servicios/ExamenServicio.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 
 public interface ExamenServicio {
     Optional<Examen> findExamenPorNombre(String nombre);
+    Examen findExamenPorNombreConPreguntas(String nombre);
 }
