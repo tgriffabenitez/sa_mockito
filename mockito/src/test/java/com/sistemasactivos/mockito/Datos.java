e1bef608c9c26674340cad2a8dbcae4b4c6ad140
tgriffabenitez <tgriffabenitez@gmail.com>
2023-05-25 17:39:08 -0300

    clase para guardar los valores que van a ser testeados. Esto es util ya que no hay que repetir codigo en cada metodo

---
+++ mockito/src/test/java/com/sistemasactivos/mockito/Datos.java
@@ -0,0 +1,22 @@
+package com.sistemasactivos.mockito;
+
+import com.sistemasactivos.mockito.modelos.Examen;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Datos {
+    public final static List<Examen> EXAMENES = Arrays.asList(
+            new Examen(5L, "Matemáticas"),
+            new Examen(6L, "Lenguaje"),
+            new Examen(7L, "Historia")
+    );
+
+    public final static List<String> PREGUNTAS = Arrays.asList(
+            "Aritmetica",
+            "Integrales",
+            "Derivadas",
+            "Trigonometria",
+            "Geometria"
+    );
+}
